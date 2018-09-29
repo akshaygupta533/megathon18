50b2d4256f4b6ac8668845401c0204e2a119f39d
Atirek Kumar <32260660+atirek-ak@users.noreply.github.com>
2018-09-30 04:00:59 +0530

    Add files via upload

---
+++ Entity.py
@@ -0,0 +1,43 @@
+#module for player
+
+class Character(self):
+    def __init__(self, health, canThrow, speedX, speedY, x, y, armour, icon):
+        self.health = health
+        self.canThrow = canThrow
+        self.speedX = speedX
+        self.speedY = speedY
+        self.x = x
+        self.y = y
+        self.armour = armour
+        self.icon = icon
+
+    def onHit(self, distance):
+        self.health-=(1/(1+distance))*0.5/self.armour
+
+
+class Enemy1(Character):
+    def __init__(self):
+        super().__init__(100, True, 5, 5, randInt(5, 1195), randInt(5, 595), 10, pygame.image.load("Enemy1.png"))
+
+
+class Enemy2(Character):
+    def __init__(self):
+        super().__init__(150, True, 10, 10, randInt(5, 1195), randInt(5, 595), 15, pygame.image.load("Enemy1.png"))
+
+
+class Player(Character):        
+    def __init__(self):
+        super().__init__(100, True, 5, 5, 5,5, pygame.image.load("Player.png"))
+
+def movement(self, dir):
+        if dir == 'a':
+            self.x -= self.speed
+        elif dir == 'd':
+            self.x += self.speed
+        elif dir == 'w':
+            self.y += self.speed    
+        elif dir == 's':
+            self.y -= self.speed
+
+
+
